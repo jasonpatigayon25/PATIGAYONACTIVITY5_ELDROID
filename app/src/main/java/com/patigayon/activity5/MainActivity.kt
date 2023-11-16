d5688684c79a0ad90363f81bd0750f08c1fd974f
Markymy <rocabertemarky09@gmail.com>
2023-11-16 22:50:54 +0800

    activity 5

--- app/src/main/java/com/patigayon/activity5/MainActivity.kt
+++ app/src/main/java/com/patigayon/activity5/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.patigayon.activity5
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.patigayon.activity5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val buttonNavigate = findViewById<Button>(R.id.button_navigate)
-        buttonNavigate.setOnClickListener {
+        binding.buttonNavigate.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/patigayon/activity5/SecondActivity.kt
+++ app/src/main/java/com/patigayon/activity5/SecondActivity.kt
@@ -4,141 +4,124 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.location.Location
-import android.os.Build
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
-import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.LocationServices
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
+import com.patigayon.activity5.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
-
-    companion object {
-        private const val PERMISSION_REQUEST_CODE = 1
-    }
+    private lateinit var binding: ActivitySecondBinding
 
     private val startCamera = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == RESULT_OK) {
+        if (result.resultCode == AppCompatActivity.RESULT_OK) {
+            val imageBitmap = result.data?.extras?.get("data") as Bitmap
 
-            val imageBitmap = result.data?.extras?.getParcelable<Bitmap>("data")
             Toast.makeText(this, "Picture taken successfully!", Toast.LENGTH_LONG).show()
         }
     }
 
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+        when {
+            isGranted -> {
+                when (currentPermissionRequest) {
+                    Manifest.permission.CAMERA -> takePicture()
+                    Manifest.permission.ACCESS_FINE_LOCATION -> getLocation()
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE -> performStorageOperation()
+                }
+            }
+            else -> Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private var currentPermissionRequest: String? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<Button>(R.id.button_take_picture).setOnClickListener {
-            handlePermission(Manifest.permission.CAMERA)
+        binding.buttonTakePicture.setOnClickListener {
+            currentPermissionRequest = Manifest.permission.CAMERA
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
 
-        findViewById<Button>(R.id.button_get_location).setOnClickListener {
-            handlePermission(Manifest.permission.ACCESS_FINE_LOCATION)
+        binding.buttonGetLocation.setOnClickListener {
+            currentPermissionRequest = Manifest.permission.ACCESS_FINE_LOCATION
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
 
-        findViewById<Button>(R.id.button_write_storage).setOnClickListener {
-            handlePermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        binding.buttonReadStorage.setOnClickListener {
+            currentPermissionRequest = Manifest.permission.WRITE_EXTERNAL_STORAGE
+            requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
         }
     }
 
-    private fun handlePermission(permission: String) {
-        if (hasPermission(permission)) {
-            performActionBasedOnPermission(permission)
+    private fun takePicture() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startCamera.launch(cameraIntent)
+    }
+
+    private fun getLocation() {
+        val locationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Location permission is not granted", Toast.LENGTH_SHORT).show()
+            return
+        }
+        locationClient.lastLocation.addOnSuccessListener { location ->
+            location?.let {
+                Toast.makeText(this, "Lat: ${it.latitude}, Lon: ${it.longitude}", Toast.LENGTH_LONG).show()
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this, "Failed to get location", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun performStorageOperation() {
+        Toast.makeText(this, "Performing action with storage permission", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun requestPermission(permission: String, rationale: String, requestCode: Int) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+            AlertDialog.Builder(this)
+                .setTitle("Permission Required")
+                .setMessage(rationale)
+                .setPositiveButton("OK") { _, _ ->
+                    ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+                }
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show()
         } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (shouldShowRequestPermissionRationale(permission)) {
-                    explainPermissionBeforeRequesting(permission)
-                }
-                requestPermissions(arrayOf(permission), PERMISSION_REQUEST_CODE)
-            } else {
-                Toast.makeText(this, "OS Version does not support this feature", Toast.LENGTH_LONG).show()
-            }
+            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
         }
     }
 
-    private fun hasPermission(permission: String): Boolean {
-        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun explainPermissionBeforeRequesting(permission: String) {
-
-        val message = when (permission) {
-            Manifest.permission.CAMERA -> "Camera access is required to take pictures."
-            Manifest.permission.ACCESS_FINE_LOCATION -> "Location access is required to retrieve your current position."
-            Manifest.permission.WRITE_EXTERNAL_STORAGE -> "Write access is required to save data on your device."
-            else -> "Additional permissions required."
-        }
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-    }
-
-    private fun performActionBasedOnPermission(permission: String) {
-        when (permission) {
-            Manifest.permission.CAMERA -> {
-                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startCamera.launch(cameraIntent)
-            }
-            Manifest.permission.ACCESS_FINE_LOCATION -> {
-                if (hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
-                    try {
-                        val locationClient = LocationServices.getFusedLocationProviderClient(this)
-                        locationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                            location?.let {
-                                Toast.makeText(this, "Lat: ${it.latitude}, Lon: ${it.longitude}", Toast.LENGTH_LONG).show()
-                            } ?: Toast.makeText(this, "Location is not available", Toast.LENGTH_LONG).show()
-                        }.addOnFailureListener {
-                            Toast.makeText(this, "Failed to get location", Toast.LENGTH_LONG).show()
-                        }
-                    } catch (e: SecurityException) {
-                        Toast.makeText(this, "Location permission not granted", Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-            Manifest.permission.WRITE_EXTERNAL_STORAGE -> {
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q && Environment.MEDIA_MOUNTED == Environment.getExternalStorageState()) {
-                    val externalStorageDir = Environment.getExternalStorageDirectory()
-                    val file = File(externalStorageDir, "myFile.txt")
-                    try {
-                        val fileOutputStream = FileOutputStream(file)
-                        fileOutputStream.use { stream ->
-                            stream.write("Data to write to file".toByteArray())
-                        }
-                        Toast.makeText(this, "File written to storage", Toast.LENGTH_LONG).show()
-                    } catch (e: IOException) {
-                        Toast.makeText(this, "Error writing file: ${e.message}", Toast.LENGTH_LONG).show()
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            permissions.forEachIndexed { index, permission ->
+                if (grantResults[index] == PackageManager.PERMISSION_GRANTED) {
+                    when (permission) {
+                        Manifest.permission.CAMERA -> takePicture()
+                        Manifest.permission.ACCESS_FINE_LOCATION -> getLocation()
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE -> performStorageOperation()
                     }
                 } else {
-                    Toast.makeText(this, "External storage not available", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Permission denied: $permission", Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            PERMISSION_REQUEST_CODE -> {
-                grantResults.forEachIndexed { index, result ->
-                    val permission = permissions[index]
-                    if (result == PackageManager.PERMISSION_GRANTED) {
-                        performActionBasedOnPermission(permission)
-                    } else {
-                        Toast.makeText(this, "Permission denied for: $permission", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-        }
+    companion object {
+        private const val PERMISSION_REQUEST_CODE = 100
     }
+
 }
